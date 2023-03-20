26934c4df23831dbffbe42389de8a4edf684b365
juanlo017 <54325276+juanlo017@users.noreply.github.com>
2023-03-20 18:46:28 +0100

    Update PartidoController.java

--- src/main/java/org/springframework/samples/volleymate/partido/PartidoController.java
+++ src/main/java/org/springframework/samples/volleymate/partido/PartidoController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PartidoController {
