5b4a268883663f14faaeee136eabbbcdbc3b1695
joscasvaz <jicastrotic99@gmail.com>
2023-03-20 20:46:54 +0100

    fix-#046: pasar datos al jsp

--- src/main/java/org/springframework/samples/volleymate/partido/PartidoController.java
+++ src/main/java/org/springframework/samples/volleymate/partido/PartidoController.java
@@ -151,14 +151,14 @@ public class PartidoController {
 		}
 	}*/
 
-  @GetMapping(value = "/partido/listaPartidos?lugar=${place}")
+  @GetMapping(value = "/partido/listaPartidos?lugar={place}")
 	public String showPartidosSexo(Map<String, Object> model, @PathVariable("place") String place) {
 		
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		
 		if (auth != null){
 			Set<Partido> partidosL = partidoService.getPartidosByLugar(place);
-			model.put("partidosSexo", partidosL);
+			model.put("partidos", partidosL);
 			return VIEW_LISTA_PARTIDOS;
 		} else {
 			return "redirect:/";
