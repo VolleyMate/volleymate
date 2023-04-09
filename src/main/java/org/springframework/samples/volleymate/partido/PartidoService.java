22ffd8011414db63b3de1543258e48036b0ec771
barbaat <barbatrejofco@gmail.com>
2023-04-09 19:45:26 +0200

    fix

--- src/main/java/org/springframework/samples/volleymate/partido/PartidoService.java
+++ src/main/java/org/springframework/samples/volleymate/partido/PartidoService.java
@@ -45,11 +45,7 @@ public class PartidoService {
 
 	@Transactional(rollbackFor = IllegalArgumentException.class)
 	public void save(Partido partido) throws DataAccessException, IllegalArgumentException {
-		// if(partido.getFecha().isBefore(LocalDateTime.now())) {
-		// 	throw new IllegalArgumentException();
-		// }else {
-			partidoRepository.save(partido);
-		//}
+		partidoRepository.save(partido);
 	}
 
 	@Transactional
@@ -101,9 +97,6 @@ public class PartidoService {
         } else if (tipoPartido != null) {
 			Page<Partido> partidosPorTipo = partidoPageRepository.findByTipo(pageable, tipoPartido);
 			return partidosPorTipo;
-        // } else if (ciudad != null && !ciudad.isEmpty()) {
-		// 	Page<Partido> partidosPorCiudad = partidoPageRepository.findByCiudad(pageable, ciudad);
-		// 	return partidosPorCiudad;
         } else {
 			return partidosSinFiltrar;
 		}
