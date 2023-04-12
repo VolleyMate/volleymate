5d3946caddd5316166f39557f7710743d6c9afd0
alecarnun <alecarnun@alum.us.es>
2023-04-12 18:15:38 +0200

    feature #88: PayPal. Modificacion servicio

--- src/main/java/org/springframework/pagos/PaypalConfig.java
+++ src/main/java/org/springframework/pagos/PaypalConfig.java
@@ -1,9 +1,13 @@
 package org.springframework.pagos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
 
 @Configuration
@@ -17,9 +21,22 @@ public class PaypalConfig {
 	private String mode;
 
 	@Bean
+	public Map<String, String> paypalSdkConfig(){
+		Map<String, String> configMap = new HashMap<>();
+		configMap.put("mode","mode");
+		return configMap;
+	}
+
+	@Bean
+	public OAuthTokenCredential oAuthTokenCredential(){
+		return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+	}
+	@Bean
 	public APIContext apiContext() throws PayPalRESTException {
-		APIContext context = new APIContext(clientId, clientSecret, mode);
-		return context;
+		// APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+		APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+		context.setConfigurationMap(paypalSdkConfig());
+		return apiContext();
 	}
 
 }
