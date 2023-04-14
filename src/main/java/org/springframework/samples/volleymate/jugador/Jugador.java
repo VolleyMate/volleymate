c48e73a8c4095d8e999e14565cecbdd3e9e68dc6
joscasvaz <jicastrotic99@gmail.com>
2023-04-14 17:38:41 +0200

    fix: import

--- src/main/java/org/springframework/samples/volleymate/jugador/Jugador.java
+++ src/main/java/org/springframework/samples/volleymate/jugador/Jugador.java
@@ -20,6 +20,8 @@ import org.springframework.samples.volleymate.model.Person;
 import org.springframework.samples.volleymate.partido.Partido;
 import org.springframework.samples.volleymate.user.User;
 import org.springframework.samples.volleymate.valoracion.Valoracion;
+import org.springframework.samples.volleymate.logros.Logro;
+
 import lombok.Getter;
 import lombok.Setter;
 
