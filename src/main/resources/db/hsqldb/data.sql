0fbcf8b20fd0024593e24e462e6ac2dc5f65dc25
jorsilman <jorsilman@alum.us.es>
2023-04-17 12:47:18 +0200

    fix: Arreglado ver perfil

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO users(username,password,enabled,correo) VALUES ('admin1','4dm1n',TRUE,'admin@gmail.com');
 INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
-INSERT INTO jugadores(id, first_name, last_name, ciudad, telephone, sexo, image, username, volleys, premium) VALUES (1,'Admin','Admin','Sevilla','666666666','MASCULINO','https://img.freepik.com/vector-premium/icono-perfil-avatar_188544-4755.jpg?w=2000','admin1', 10000000, TRUE);
+INSERT INTO jugadores(id, first_name, last_name, ciudad, telephone, sexo, image, username, volleys, premium, fecha_fin_premium) VALUES (1,'Admin','Admin','Sevilla','666666666','MASCULINO','https://img.freepik.com/vector-premium/icono-perfil-avatar_188544-4755.jpg?w=2000','admin1', 10000000, TRUE, '2024-01-01');
 
 INSERT INTO users(username,password,enabled,correo) VALUES ('jorsilman','jorsilman',TRUE,'jorge@gmail.com');
 INSERT INTO authorities(id,username,authority) VALUES (2,'jorsilman','admin');
-INSERT INTO jugadores(id, first_name, last_name, ciudad, telephone, sexo, image, username, volleys, premium) VALUES (2,'Jorge','Sillero','Sevilla','657236154','MASCULINO','https://st1.uvnimg.com/e0/40/ab16af48465f804d56e0c2a7ccf4/gettyimages-1240189250.jpg','jorsilman', 1000, TRUE);
+INSERT INTO jugadores(id, first_name, last_name, ciudad, telephone, sexo, image, username, volleys, premium, fecha_fin_premium) VALUES (2,'Jorge','Sillero','Sevilla','657236154','MASCULINO','https://st1.uvnimg.com/e0/40/ab16af48465f804d56e0c2a7ccf4/gettyimages-1240189250.jpg','jorsilman', 1000, TRUE, '2024-01-01');
 
 INSERT INTO users(username,password,enabled,correo) VALUES ('barba','barba',TRUE,'barba@gmail.com');
 INSERT INTO authorities(id,username,authority) VALUES (3,'barba','admin');
