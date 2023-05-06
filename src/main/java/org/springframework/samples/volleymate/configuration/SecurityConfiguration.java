089c8e012656329a9b53350b379e596ad7a6d5a8
jorsilman <jorsilman@alum.us.es>
2023-05-06 13:45:45 +0200

    fix #253: redirecciona bien https

--- src/main/java/org/springframework/samples/volleymate/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/volleymate/configuration/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package org.springframework.samples.volleymate.configuration;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +15,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -56,6 +63,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				 	.formLogin()
 				 	/*.loginPage("/login")*/
 				 	.failureUrl("/login-error")
+					.successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            response.sendRedirect("https://volleymate-ppl.ew.r.appspot.com/");
+                        }
+                    })
 				.and()
 					.logout()
 						.logoutSuccessUrl("/"); 
