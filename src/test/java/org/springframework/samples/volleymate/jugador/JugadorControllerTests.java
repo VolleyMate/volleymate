925f27966e484eac28dd8575eabca9e942949e1b
jorsilman <jorsilman@alum.us.es>
2023-05-13 20:15:17 +0200

    test JugadorController

--- src/test/java/org/springframework/samples/volleymate/jugador/JugadorControllerTests.java
+++ src/test/java/org/springframework/samples/volleymate/jugador/JugadorControllerTests.java
@@ -8,7 +8,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -23,7 +27,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.samples.volleymate.aspecto.Aspecto;
 import org.springframework.samples.volleymate.aspecto.AspectoService;
 import org.springframework.samples.volleymate.centro.Centro;
 import org.springframework.samples.volleymate.centro.CentroService;
@@ -31,12 +38,15 @@ import org.springframework.samples.volleymate.configuration.SecurityConfiguratio
 import org.springframework.samples.volleymate.jugador.Jugador;
 import org.springframework.samples.volleymate.jugador.JugadorController;
 import org.springframework.samples.volleymate.jugador.JugadorService;
+import org.springframework.samples.volleymate.logro.Logro;
 import org.springframework.samples.volleymate.logro.LogroService;
 import org.springframework.samples.volleymate.mensaje.Mensaje;
 import org.springframework.samples.volleymate.mensaje.MensajeService;
 import org.springframework.samples.volleymate.pagos.PaymentService;
+import org.springframework.samples.volleymate.partido.Partido;
 import org.springframework.samples.volleymate.partido.PartidoPageService;
 import org.springframework.samples.volleymate.partido.PartidoService;
+import org.springframework.samples.volleymate.solicitud.Solicitud;
 import org.springframework.samples.volleymate.solicitud.SolicitudService;
 import org.springframework.samples.volleymate.user.Authorities;
 import org.springframework.samples.volleymate.user.AuthoritiesService;
@@ -44,11 +54,13 @@ import org.springframework.samples.volleymate.user.User;
 import org.springframework.samples.volleymate.user.UserService;
 import org.springframework.samples.volleymate.valoracion.ValoracionService;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @WebMvcTest(controllers = JugadorController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class, properties = {
-    "spring.jpa.database-platform: org.hibernate.dialect.H2Dialect" })
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class JugadorControllerTests {
+    "database=hsqldb",
+"spring.h2.console.enabled=true" })
+class JugadorControllerTests {
 
     @MockBean
 	private JugadorService jugadorService;
@@ -100,6 +112,28 @@ public class JugadorControllerTests {
 		george.setUser(user);
         george.setVolleys(200);
         george.setSexo(Sexo.MASCULINO);
+		george.setCiudad(Ciudad.BADAJOZ);
+		george.setFechaFinPremium(LocalDateTime.parse("2024-01-01T00:00:00"));
+		george.setFechaInicioPremium(LocalDateTime.parse("2020-01-01T00:00:00"));
+
+		george.setImage("'https://img.freepik.com/vector-premium/icono-perfil-avatar_188544-4755.jpg?w=2000'");
+		george.setLogros(new ArrayList<Logro>());
+		george.setAspectos(new ArrayList<Aspecto>());
+		george.setPremium(false);
+		george.setTelephone("123456789");
+		george.setPartidos(new HashSet<Partido>());
+
+
+		given(this.jugadorService.findJugadorById(1)).willReturn(george);
+		given(this.jugadorService.findJugadorByUsername("Test")).willReturn(george);
+		Page<Partido> paginaVacia = new PageImpl<>(Collections.emptyList());
+		given(this.partidoService.buscarPartidosPorJugador(0, george)).willReturn(paginaVacia);
+		given(this.jugadorService.findPartidosByJugadorId(1)).willReturn(new ArrayList<Partido>());
+		given(this.solicitudService.findSolicitudesATusPartidos(george)).willReturn(new HashSet<Solicitud>());
+		given(this.solicitudService.findTusSolicitudes(george)).willReturn(new HashSet<Solicitud>());
+
+		given(this.jugadorService.listAll(null, 0)).willReturn(new ArrayList<>(Arrays.asList(george)));
+
 		
 
 		// ==== Jugador2 ==== //
@@ -122,6 +156,9 @@ public class JugadorControllerTests {
         barba.setVolleys(200);
         barba.setSexo(Sexo.MASCULINO);
 
+		given(this.jugadorService.findJugadorById(2)).willReturn(barba);
+		given(this.jugadorService.findJugadorByUsername("Test2")).willReturn(barba);
+
 		// ==== Jugador3 ==== //
 		User user3 = new User();
 		user3.setUsername("Test3");
@@ -140,14 +177,40 @@ public class JugadorControllerTests {
 		jugador3.setVolleys(200);
 		jugador3.setSexo(Sexo.MASCULINO);
 
-        userService.saveUser(user);
-        userService.saveUser(user2);
-        userService.saveUser(user3);
-		jugadorService.saveJugador(jugador3);
-		jugadorService.saveJugador(barba);
-		jugadorService.saveJugador(george);
+		given(this.jugadorService.findJugadorById(3)).willReturn(jugador3);
+		given(this.jugadorService.findJugadorByUsername("Test3")).willReturn(jugador3);
+		given(this.aspectoService.findAllAspectos()).willReturn(new ArrayList<Aspecto>());
+		given(this.jugadorService.esAdmin(jugador3)).willReturn(true);
 
 	}
+
+	@WithMockUser(value = "spring", username = "Test3", authorities = "jugador")
+    @Test
+	void testInitCreationForm() throws Exception {
+		mockMvc.perform(get("/jugadores/new")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/crearJugador"));
+	}
+
+	/*@WithMockUser(value = "spring", username = "Test3", authorities = "jugador")
+    @Test
+	void testProcessCreationFormSuccess() throws Exception {
+		mockMvc.perform(post("/jugadores/new")
+		.param("firstName", "Joe")
+		.param("lastName", "Bloggs")
+		.param("sexo", "MASCULINO")
+		.param("fechaNacimiento", "1999/01/01")
+		.param("volleys", "200")
+		.param("telephone", "123456789")
+		.param("ciudad", "SEVILLA")
+		.param("user.username", "Test4")
+		.param("user.password", "123")
+		.param("user.correo", "test4@us.es")
+		.param("user.enabled", "true")
+		.param("user.authorities", "jugador")).with(csrf())
+				.andExpect(status().isOk());
+	}*/
+				
+
 
     @WithMockUser(value = "spring", username = "Test3", authorities = "jugador")
     @Test
@@ -155,4 +218,51 @@ public class JugadorControllerTests {
         mockMvc.perform(get("/jugadores/1")).andExpect(status().isOk())
                 .andExpect(view().name("jugadores/detallesJugador"));
     }
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowJugadorAutenticado() throws Exception {
+		mockMvc.perform(get("/jugadores")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/detallesJugador"));
+	}
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowMisPartidos() throws Exception {
+		mockMvc.perform(get("/jugadores/mispartidos")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/misPartidos"));
+	}
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowVistaNotificaciones() throws Exception {
+		mockMvc.perform(get("/jugadores/notificaciones")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/notificacionesJugador"));
+	}
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowJugadores() throws Exception {
+		mockMvc.perform(get("/listaJugadores")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/listaJugadores"));
+	}
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowVistaMisAspectos() throws Exception {
+		mockMvc.perform(get("/misAspectos")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/listaMisAspectos"));
+	}
+
+	@WithMockUser(value = "spring", username = "Test", authorities = "jugador")
+    @Test
+	void testShowTerminos() throws Exception {
+		mockMvc.perform(get("/terminos")).andExpect(status().isOk())
+				.andExpect(view().name("jugadores/terminos"));
+	}
+
+	
+
+	
+
 }
